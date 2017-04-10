b09d0b838f02d6e1dd2162e503845813af944589
Alex Murray <namurray1@gmail.com>
2017-04-10 18:44:35 -0400

    made several code changes and updates. Still undable to get code working

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
-CREATE TABLE
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+/*table goes here*/
+CREATE TABLE burgers (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN,
+    DATE DEFAULT CURRENT TIMESTAMP),
+    PRIMARY KEY (id)
+    );
--- server.js
+++ server.js
@@ -1,4 +1,27 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('bodyParser');
+
 var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var handleBars = require("express-handlebars"); //review this line, Alex
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+	console.log("Server listening on port " + port);
+})
